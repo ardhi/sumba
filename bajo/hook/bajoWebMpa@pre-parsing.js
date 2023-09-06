b3250c85001af29dfe0ca2c77fb9051e8845522b
Ardhi Lukianto <ardhi@lukianto.com>
2023-09-06 21:43:23 +0700

    Add dark mode

--- bajo/hook/bajoWebMpa@pre-parsing.js
+++ bajo/hook/bajoWebMpa@pre-parsing.js
@@ -1,10 +1,12 @@
 import checkUserId from '../../lib/check-user-id.js'
 import checkTheme from '../../lib/check-theme.js'
 import checkLang from '../../lib/check-lang.js'
+import checkDarkMode from '../../lib/check-dark-mode.js'
 
 const onRequest = {
   level: 10,
   handler: async function (ctx, req, reply) {
+    await checkDarkMode.call(this, req, reply)
     await checkLang.call(this, req, reply)
     await checkTheme.call(this, req, reply)
     await checkUserId.call(this, req, reply, 'bajoWebMpa')
