885220ec211a96c56b4b6439616ce149f8e4499e
Ardhi Lukianto <ardhi@lukianto.com>
2024-10-25 02:47:44 +0700

    Updates

--- bajo/hook/waibu-mpa.sumba@after-build-locals.js
+++ bajo/hook/waibu-mpa.sumba@after-build-locals.js
@@ -4,6 +4,7 @@ async function afterBuildLocals (locals, req) {
   if (req.user) {
     items.push({ icon: 'person', tooltip: 'Your Profile', href: routePath('sumba:/profile') })
     items.push({ icon: 'key', tooltip: 'Change Password', href: routePath('sumba:/change-password') })
+    items.push({ component: 'navItemSignout', tooltip: 'Signout', bottom: true })
   } else {
     items.push({ icon: 'signin', tooltip: 'Signin', href: routePath('sumba:/signin') })
     items.push({ icon: 'key', tooltip: 'Forgot Password', href: routePath('sumba:/forgot-password') })
