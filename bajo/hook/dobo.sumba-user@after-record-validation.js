888214c3c254699954ad882e7577cde0df477d69
Ardhi Lukianto <ardhi@lukianto.com>
2025-01-12 21:06:16 +0700

    Bug fix for empty token

--- bajo/hook/dobo.sumba-user@after-record-validation.js
+++ bajo/hook/dobo.sumba-user@after-record-validation.js
@@ -1,9 +1,9 @@
-async function doboSumbaUserBeforeRecordUpdate (body, options) {
+async function doboSumbaUserAfterRecordValidation (body, options) {
   const { isBcrypt, isMd5, hash } = this.app.bajoExtra
   const { has } = this.app.bajo.lib._
 
   if (has(body, 'password') && !isBcrypt(body.password)) body.password = await hash(body.password, 'bcrypt')
-  if (has(body, 'token') && !isMd5(body.token)) body.token = await hash(body.token)
+  if (has(body, 'token') && !isMd5(body.token)) body.token = await hash(body.password)
 }
 
-export default doboSumbaUserBeforeRecordUpdate
+export default doboSumbaUserAfterRecordValidation
