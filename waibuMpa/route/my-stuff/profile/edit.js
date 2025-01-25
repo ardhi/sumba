be2f1301f3a853838c0e9e6342b2a98e62a13203
Ardhi Lukianto <ardhi@lukianto.com>
2025-01-25 19:35:29 +0700

    Updates

--- waibuMpa/route/my-stuff/profile/edit.js
+++ waibuMpa/route/my-stuff/profile/edit.js
@@ -15,14 +15,16 @@ const profile = {
           const opts = { req, setField: 'profile', setFile: 'main.png' }
           await attachmentCopyUploaded('SumbaUser', req.user.id, opts)
         } else {
-          form = await recordUpdate('SumbaUser', req.user.id, form, { noFlash: true })
+          const resp = await recordUpdate({ req, model: 'SumbaUser', id: req.user.id, body: form, options: { noFlash: true, hidden: [] } })
+          form = resp.data
           req.flash('notify', req.t('Your profile is successfully updated'))
         }
       } catch (err) {
+        console.log(err)
         error = err
       }
     }
-    form.token = await hash(form.token)
+    if (form.token) form.token = await hash(form.token)
     return reply.view('sumba.template:/my-stuff/profile/edit.html', { form, error })
   }
 }
